c88b70c8e023cd73263c5c9895ca353c8858f99a
Jacobo Coa <jacobo.daniel.coa@gmail.com>
2021-03-20 20:25:37 -0300

    created collection opertaion

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -8,8 +8,8 @@ fun main(){
     }
 
     println("--------------------")
-    println("Maior salario: ${salarios.max()}")
-    println("Menor salario: ${salarios.min()}")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
     println("Média salarias: ${salarios.average()}")
 
     // No filter é necessário passar um predicado, ou seja algo que vai nos dar uma expressão e sendo verdade, vai filtrando e alocando o resultado
@@ -18,4 +18,16 @@ fun main(){
     println("--------------------")
 
     salariosMaiorQue2500.forEach{ println(it)}
+
+    println("--------------------")
+    //val s = salarios.filter{it > 2000 && it < 5000}
+    println(salarios.count{ it in 2000.0..5000.0})
+
+    println("--------------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("--------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
 }
