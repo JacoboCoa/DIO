439cf1c16447e5f9473f811a5d433daa593e8984
Jacobo Coa <jacobo.daniel.coa@gmail.com>
2021-03-24 21:03:28 -0300

    Starting the project with the ResultActivity.kt creation

---
+++ calculadoraIMC/app/src/main/java/one/digitalinnovation/calculadoraimc/ResultActivity.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
