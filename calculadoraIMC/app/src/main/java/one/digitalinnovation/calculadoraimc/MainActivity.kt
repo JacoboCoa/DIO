db8e3f8c27609a4860e9b3fb9a96deec061f7e5e
Jacobo Coa <jacobo.daniel.coa@gmail.com>
2021-04-07 23:48:31 -0300

    creation main linear and relative layout

--- calculadoraIMC/app/src/main/java/one/digitalinnovation/calculadoraimc/MainActivity.kt
+++ calculadoraIMC/app/src/main/java/one/digitalinnovation/calculadoraimc/MainActivity.kt
@@ -3,6 +3,10 @@ package one.digitalinnovation.calculadoraimc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +14,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.w("lifecycle", "CREATE - estou criando a tela")
         //Destroy a activity
-        finish()
+        //finish()
+        setListeners()
     }
 
+    private fun setListeners(){
+        // é sempre importante checar a visibilidade da view, então é necessario colocar o ?
+        alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        pesoEDT?.doOnTextChanged(){ text, _, _, _ ->
+            titleTXT?.text = text
+        }
+        calculateBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if(peso != null && altura != null){
+            val imc = peso/ (altura * altura)
+            titleTXT.text = "Seu IMC é:\n%.2f".format(imc)
+        }
+    }
+    /*
     override fun onStart() {
         super.onStart()
         Log.w("lifecycle", "START - deixei a tela visível")
@@ -41,5 +68,5 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.w("lifecycle", "DESTROY - oh não! a tela foi destruída")
-    }
+    }*/
 }
